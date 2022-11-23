001fbfc2ecf62233dd51916aff768dcd6919b0f1
William Lowrimore <wlowrimore@gmail.com>
2022-11-23 13:08:37 -0600

    adds show edit form state

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+      {!isEditing && <button onClick={startEditingHandler}>AddNew Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} 
+                                 onCancel={stopEditingHandler}/>}
+    </div> 
   )
 }
 
